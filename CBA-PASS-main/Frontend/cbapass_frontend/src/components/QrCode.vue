a80826a6d56e27e62ab22c9fd4dddbd73a7bb161
Santiago Salamanca <sansalrus@gmail.com>
2025-07-18 01:29:07 -0500

    Commit final

---
+++ CBA-PASS-main/Frontend/cbapass_frontend/src/components/QrCode.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="card">
+    <h2>Carnet Virtual</h2>
+    <p><strong>Nombre:</strong> {{ persona.nombre }}</p>
+    <p><strong>Documento:</strong> {{ persona.documento }}</p>
+    <p><strong>Email:</strong> {{ persona.email }}</p>
+    <p><strong>Contraseña:</strong> {{ persona.contraseña }}</p>
+    <img :src="'data:image/png;base64,' + qr" alt="QR Code">
+
+    <button @click="cancelar">Cancelar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    persona: {
+      type: Object,
+      required: true
+    },
+    qr: {
+      type: String,
+      required: true
+    }
+  },
+  emits: ['cancelar'],
+  methods: {
+    cancelar() {
+      this.$emit('cancelar')
+      window.scrollTo({ top: 0, behavior: 'smooth' })  // ir al inicio
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.card {
+  border: 2px solid #0077cc;
+  padding: 20px;
+  border-radius: 12px;
+  width: 300px;
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 20px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+}
+.card img {
+  margin-top: 15px;
+  width: 150px;
+  height: 150px;
+}
+button {
+  margin-top: 20px;
+  padding: 8px 16px;
+  background-color: #cc0000;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #a00000;
+}
+</style>
---
+++ CBA-PASS-main/Frontend/cbapass_frontend/src/views/Carnet.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="Caja">
+      <div class="qr">
+          <qrcode-vue :value="qrData" :size="200" level="h" />
+      </div>
+      <div>
+        <h4>Tiempo de expiracion: {{ tiempoRestante }}</h4>
+        <button @click="Salir" >Salir</button>
+      </div>
+    </div>
+</template>
+
+<script setup >
+import { useAuthStore } from '@/stores/auth'
+import QrcodeVue from 'qrcode.vue'
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const tiempoRestante = ref(300) // 5 minutos = 300 segundos
+const authStore = useAuthStore()
+
+onMounted(() => {
+  // Temporizador de cuenta regresiva cada segundo
+  const intervalo = setInterval(() => {
+    tiempoRestante.value--
+    if (tiempoRestante.value <= 0) {
+      clearInterval(intervalo)
+      router.push('/') // Redirigir al inicio
+    }
+  }, 1000)
+})
+
+const Salir = () => {
+  authStore.logout()
+  router.push('/')
+}
+
+</script>
+
+<style scoped>
+
+.Caja{
+  border-radius: 20px;
+  border:  2px solid black;
+  margin: 10rem;
+  padding: 3rem;
+  background: white;
+}
+.qr{
+  display: flex;
+  justify-content: center;
+  padding-bottom: 5rem;
+}
+button{
+  background: red;
+  font-size: larger;
+  width: 5rem;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+}
+
+
+</style>
